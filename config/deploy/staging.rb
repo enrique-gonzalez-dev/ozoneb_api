39a0df2d698c52968409386cc7f4eb136615ce30
Enrique Gonzalez <enrique.gonzan@gmail.com>
2025-12-10 14:06:58 -0600

    Resolve deploy issues

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,15 @@
 set :stage, :staging
 set :rails_env, 'production'
 
+# Environment variables for the app
+set :default_env, {
+  'RAILS_ENV' => 'production',
+  'OZONEB_API_DATABASE_PASSWORD' => 'OzoneBenefits2025',
+  'RAILS_SERVE_STATIC_FILES' => 'true',
+  'RAILS_LOG_TO_STDOUT' => 'true',
+  'RAILS_MAX_THREADS' => '5'
+}
+
 # Replace with your server IP and user
 server '18.119.132.48', user: 'ubuntu', roles: %w{app db web}, ssh_options: {
   user: 'ubuntu',
